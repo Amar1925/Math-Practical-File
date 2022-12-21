fe7c48d9224b24a3a67ef932b63663076465386e
AmarKumar1912 <119916402+AmarKumar1912@users.noreply.github.com>
2022-12-21 10:31:14 +0530

    Add files via upload

---
+++ Prac 1 math matrix transpose.py
@@ -0,0 +1,9 @@
+import numpy as np
+              
+# make matrix with numpy
+gfg = np.matrix('[4, 1; 12, 3]')
+              
+# applying matrix.transpose() method
+geek = gfg.transpose()
+    
+print(geek)
